47ab81eff8ce79b60c7f9993418ec69d5881de44
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2023-04-06 20:04:01 -0300

    chore(toggleTabPane): add structure component and style

---
+++ src/components/toggleTabPane/toggleTabPane.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './toggleTabPane.css';
+
+function ToggleTabPane({ content }) {
+    return (
+        <div className="toggle-tab__pane toggle-tab__pane--active">
+            {content.map((card, index) => (
+                <div key={index} className="card">
+                    <img src={card.imageUrl} alt={card.title} />
+                    <h3 className="title">{card.title}</h3>
+                    <p className="description">{card.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ToggleTabPane;
