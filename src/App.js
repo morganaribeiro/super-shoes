c459435ce81af7ceebbd851476f68469df7041cb
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2023-04-06 20:02:04 -0300

    chore(toggleTab): add new component toggle tab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,77 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import ToggleTab from './components/toggleTab/toggleTab';
 
 function App() {
   const [data, setData] = useState([]);
   const carousel = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const tabs = [
+    {
+      id: 1,
+      label: 'Tab 1',
+      content: [
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 1',
+          description: 'Descrição do Card 1',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 2',
+          description: 'Descrição do Card 2',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 3',
+          description: 'Descrição do Card 3',
+        },
+      ],
+    },
+    {
+      id: 2,
+      label: 'Tab 2',
+      content: [
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 4',
+          description: 'Descrição do Card 4',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 5',
+          description: 'Descrição do Card 5',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 6',
+          description: 'Descrição do Card 6',
+        },
+      ],
+    },
+    {
+      id: 3,
+      label: 'Tab 3',
+      content: [
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 7',
+          description: 'Descrição do Card 7',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 8',
+          description: 'Descrição do Card 8',
+        },
+        {
+          imageUrl: 'https://via.placeholder.com/150x150',
+          title: 'Card 9',
+          description: 'Descrição do Card 9',
+        },
+      ],
+    },
+  ];
 
   useEffect(() => {
     fetch('http://localhost:3000/static/shoes.json')
@@ -36,36 +103,41 @@ function App() {
   if (!data || !data.length) return null;
 
   return (
-    <div className="container">
-      <div className="logo">
-        <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
-      </div>
-      <div className="carousel" ref={carousel}>
-        {data.map((item) => {
-          const { id, name, price, oldPrice, image } = item;
-          return (
-            <div className="item" key={id}>
-              <div className="image">
-                <img src={image} alt={name} />
+    <>
+      <div className="container">
+        <div className="logo">
+          <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
+        </div>
+        <div className="carousel" ref={carousel}>
+          {data.map((item) => {
+            const { id, name, price, oldPrice, image } = item;
+            return (
+              <div className="item" key={id}>
+                <div className="image">
+                  <img src={image} alt={name} />
+                </div>
+                <div className="info">
+                  <span className="name">{name}</span>
+                  <span className="oldPrice">U$ {oldPrice}</span>
+                  <span className="price">U$ {price}</span>
+                </div>
               </div>
-              <div className="info">
-                <span className="name">{name}</span>
-                <span className="oldPrice">U$ {oldPrice}</span>
-                <span className="price">U$ {price}</span>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
+        <div className="buttons">
+          <button onClick={handleLeftClick} className={`arrow ${currentIndex === 0 ? "disabled" : ""}`} disabled={currentIndex === 0}>
+            <div className="arrowLeft"></div>
+          </button>
+          <button onClick={handleRightClick} className={`arrow ${currentIndex === data.length - 1 ? "disabled" : ""}`} disabled={currentIndex === data.length - 1}>
+            <div className="arrowRight"></div>
+          </button>
+        </div>
       </div>
-      <div className="buttons">
-        <button onClick={handleLeftClick} className={`arrow ${currentIndex === 0 ? "disabled" : ""}`} disabled={currentIndex === 0}>
-          <div className="arrowLeft"></div>
-        </button>
-        <button onClick={handleRightClick} className={`arrow ${currentIndex === data.length - 1 ? "disabled" : ""}`} disabled={currentIndex === data.length - 1}>
-          <div className="arrowRight"></div>
-        </button>
+      <div className="toggle-tab-container">
+        <ToggleTab tabs={tabs} />
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/toggleTab/toggleTab.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import ToggleTabPane from '../toggleTabPane/toggleTabPane';
+import './toggleTab.css';
+
+function ToggleTab({ tabs }) {
+    const [activeTab, setActiveTab] = useState(tabs[0].id);
+
+    const handleClick = (tabId) => {
+        setActiveTab(tabId);
+    };
+
+    return (
+        <div className="toggle-tab">
+            <ul className="toggle-tab__list">
+                {tabs.map((tab) => (
+                    <li
+                        key={tab.id}
+                        className={`toggle-tab__item ${activeTab === tab.id ? 'toggle-tab__item--active' : ''
+                            }`}
+                        onClick={() => handleClick(tab.id)}
+                    >
+                        {tab.label}
+                    </li>
+                ))}
+            </ul>
+            <div className="toggle-tab__content">
+                {tabs.map((tab) => (
+                    activeTab === tab.id && (
+                        <ToggleTabPane key={tab.id} content={tab.content} />
+                    )
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ToggleTab;
